1d5cefeab81aae90422c9be46a22d2a0c659c830
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-19 00:22:26 +0300

    add basicLightbox and modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,27 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 
+const galleryContainer = document.querySelector('.gallery');
+// console.log(galleryContainer);
 
-const galleryEl = document.querySelector('.gallery');
-function create (items) {
-    return galleryItems.map(({ preview, original, description}) => {
-        return `
-         <div class="gallery__item">
-<a class="gallery__link" href="${original}">
-  <img
-    class="gallery__image"
-    src="${preview}""
-    data-source="${original}"
-    alt="${description}""
-  />
-</a>
-</div>`;
-    }).join('');
+function createGalleryItemMarkup(image) {
+  return galleryItems.map(({preview, original, description}) =>
+  {return  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`}).join('');
 }
 
-const cardm = create(galleryItems);
-galleryEl.insertAdjacentHTML('beforeend', cardm);
+const galleryImage = createGalleryItemMarkup(galleryItems);
+// console.log(galleryImage);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryImage);
+
+galleryContainer.addEventListener('click', handleGalleryContainerClick);
+
+function handleGalleryContainerClick(event) {
+    event.preventDefault();
+  console.log(event.target);
+  const descriptionValue = event.target.dataset.source;
+  console.log(descriptionValue);
+
+  const modal = basicLightbox.create(`
+<img src="${descriptionValue}">`)
+
+modal.show()
+}
+
+// import * as basicLightbox from 'basiclightbox'
+
+
+
 
