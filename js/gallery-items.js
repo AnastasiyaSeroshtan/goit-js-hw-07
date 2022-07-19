54865608a147028a8f1a1f6c883943c464092fca
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-19 17:43:49 +0300

    add image

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -66,71 +66,161 @@ export const galleryItems = [
     preview:
       'https://cdn.pixabay.com/photo/2015/07/28/22/03/fireworks-865104__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2015/07/28/22/03/fireworks-865104_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2015/07/28/22/03/fireworks-865104_1280.jpg',
     description: 'Fireworks',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2012/03/01/00/55/garden-19830__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2012/03/01/00/55/garden-19830_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2012/03/01/00/55/garden-19830_1280.jpg',
     description: 'Garden',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/italy-1835613__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/italy-1835613_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/italy-1835613_1280.jpg',
     description: 'Italy',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2014/12/24/05/02/drop-of-water-578897__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2014/12/24/05/02/drop-of-water-578897_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2014/12/24/05/02/drop-of-water-578897_1280.jpg',
     description: 'Drop of water',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581_1280.jpg',
     description: 'Beach',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2016/04/18/22/05/seashells-1337565__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2016/04/18/22/05/seashells-1337565_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2016/04/18/22/05/seashells-1337565_1280.jpg',
     description: 'Seashells',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2021/10/13/09/01/corgi-6705821__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2021/10/13/09/01/corgi-6705821_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2021/10/13/09/01/corgi-6705821_1280.jpg',
     description: 'Corgi',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_1280.jpg',
     description: 'Pink tree',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2022/07/10/20/18/raspberry-7313702__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2022/07/10/20/18/raspberry-7313702_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2022/07/10/20/18/raspberry-7313702_1280.jpg',
     description: 'Raspberry',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/10/13/11/44/cat-6706354__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/10/13/11/44/cat-6706354_1280.jpg',
+    description: 'Cat',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_1280.jpg',
+    description: 'Beach',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/04/27/20/53/goose-7161311__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/04/27/20/53/goose-7161311_1280.jpg',
+    description: 'Goose',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/07/07/07/24/clouds-7306684__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/07/07/07/24/clouds-7306684_1280.jpg',
+    description: 'Clouds',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/07/13/19/19/blue-eryngo-7319904__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/07/13/19/19/blue-eryngo-7319904_1280.jpg',
+    description: 'Blue eryngo',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/07/23/13/51/schafer-dog-4357790__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/07/23/13/51/schafer-dog-4357790_1280.jpg',
+    description: 'Schafer dog',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/04/25/13/02/flowers-7155917__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/04/25/13/02/flowers-7155917_1280.jpg',
+    description: 'Flowers',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/09/26/09/55/seal-6656983__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/09/26/09/55/seal-6656983_1280.jpg',
+    description: 'Seal',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/07/16/19/54/helenium-7325889__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/07/16/19/54/helenium-7325889_1280.jpg',
+    description: 'Helenium',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/06/25/23/41/ladybug-7284337__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/06/25/23/41/ladybug-7284337_1280.jpg',
+    description: 'Ladybug',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/07/08/02/44/pet-7308330__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/07/08/02/44/pet-7308330_1280.jpg',
+    description: 'Pet',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/06/26/14/53/berlin-7285666__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/06/26/14/53/berlin-7285666_1280.jpg',
+    description: 'Berlin',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/07/04/17/16/dove-7301617__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/07/04/17/16/dove-7301617_1280.jpg',
+    description: 'Dove',
   },
 ];
 
 
 
-// 
-// 
-// 
+
+
 
 
