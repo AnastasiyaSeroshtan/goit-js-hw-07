25bf417dd8b82fef08ccea46ea38740dc3d6e144
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-19 17:45:53 +0300

    add lazyload to task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -16,6 +16,23 @@ function createGalleryItemMarkup(image) {
 const galleryImage = createGalleryItemMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', galleryImage);
 
+if ('loading' in HTMLImageElement.prototype) {
+    console.log('Браузер поддерживает lazyload');
+    const lazyImage = document.querySelectorAll('.gallery__image');
+    
+    lazyImage.forEach(img => {
+    img.loading = 'lazy';
+    })   
+}
+else {
+    const script = document.querySelector('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+    script.integrity = 'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+    script.crossorigin = 'anonymous';
+    script.referrerpolicy = 'no-referrer';
+
+    document.body.appendChild(script);
+}
 
 const lightbox = new SimpleLightbox('.gallery a', { captionDelay: '250',
 captionPosition: 'bottom', captionsData: "alt"});
