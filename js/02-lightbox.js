069240336333ad0cc9552cf99f5e996e1008a818
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-20 23:39:40 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -26,10 +26,10 @@ if ('loading' in HTMLImageElement.prototype) {
     })  
 }
 else {
-    const script = document.querySelector('script');
-    script.crossOrigin="anonymous";
+    const script = document.createElement('script');
     script.src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
     script.integrity="sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+    script.crossOrigin="anonymous";
     script.referrerPolicy="no-referrer"
 
     document.body.appendChild(script);
